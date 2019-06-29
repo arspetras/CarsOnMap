d7ddf3c8a85a70fcf99e31a510a95bbc7e150ed2
rogovska <rogovska@uni.coventry.ac.uk>
2019-06-29 14:24:05 +0100

    Initial commit V0.1 Google map was added

---
+++ app/src/main/java/com/example/carsmap/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.carsmap;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        /*
+        map = googleMap;
+        LatLng Justai = new LatLng(54.719721,25.213366);
+        map.addMarker(new MarkerOptions().position(Justai).title("Namai"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Justai));
+        */
+    }
+}
